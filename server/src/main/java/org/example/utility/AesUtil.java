847d0441abe75896908688975d763ec283780502
kastashenkova <astashenkova.katya@gmail.com>
2026-06-24 23:00:02 +0300

    remove aes/rsa

--- server/src/main/java/org/example/utility/AesUtil.java
+++ server/src/main/java/org/example/utility/AesUtil.java
@@ -1,12 +1,10 @@
-package org.example.utility.cryptography;
+package org.example.utility;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class AesUtil {
@@ -14,12 +12,6 @@ public class AesUtil {
     private static final String CIPHER_TRANSFORMATION = "AES/GCM/NoPadding";
     private static final int IV_LENGTH_BYTES = 12;
     private static final int TAG_LENGTH_BITS = 128;
-
-    public static SecretKey generateSecretKey() throws NoSuchAlgorithmException {
-        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
-        keyGen.init(256);
-        return keyGen.generateKey();
-    }
 
     public static SecretKey getSecretKeyFromBytes(byte[] keyBytes) {
         return new SecretKeySpec(keyBytes, 0, keyBytes.length, ALGORITHM);
